bd301e8e53ad7c7873682c25469e856875e49e6a
Javier Laguna <javier.laguna@innocv.com>
2024-10-08 18:07:24 +0200

    first commit

---
+++ composeApp/src/commonMain/kotlin/dev/jlaguna/App.kt
@@ -0,0 +1,26 @@
+package dev.jlaguna
+
+import androidx.compose.runtime.*
+import coil3.ImageLoader
+import coil3.annotation.ExperimentalCoilApi
+import coil3.compose.setSingletonImageLoaderFactory
+import coil3.request.crossfade
+import coil3.util.DebugLogger
+import dev.jlaguna.ui.screens.detail.DetailScreen
+import dev.jlaguna.ui.screens.home.HomeScreen
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalCoilApi::class)
+@Composable
+@Preview
+fun App() {
+    setSingletonImageLoaderFactory { context ->
+        ImageLoader.Builder(context)
+            .crossfade(true)
+            .logger(DebugLogger())
+            .build()
+    }
+
+    // HomeScreen()
+    DetailScreen()
+}
---
+++ composeApp/src/commonMain/kotlin/dev/jlaguna/Movie.kt
@@ -0,0 +1,15 @@
+package dev.jlaguna
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val poster: String
+)
+
+val movies = (1..100).map {
+    Movie(
+        it,
+        "Movie $it",
+        poster = "https://picsum.photos/200/300?random=$it"
+    )
+}
